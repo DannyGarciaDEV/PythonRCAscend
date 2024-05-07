d90b0788ea9a110e134cf2790a847acc11150b6c
Danny Garcia Cortes <dannygarciadev@gmail.com>
2024-05-07 15:52:05 -0400

    leon first class exercises

---
+++ first_python_class.py
@@ -0,0 +1,17 @@
+#--- Easy
+# create a variable and assign it a number
+x = 20
+
+# minus 10 from that number
+x = x - 10
+
+# print that number
+print(x)
+#--- Medium
+number = int(input("Enter a number: "))
+
+# add 25 to that number
+number = number + 25
+
+# print the result
+print(number)
